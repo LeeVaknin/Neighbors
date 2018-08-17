9fe64599637c939dd7d0c5c27458485e7942c7c2
caysebix <caysebix@gmail.com>
2018-08-18 00:43:37 +0300

    fixed datacontext namespace in migration

--- Neighbors/Migrations/NeighborsContextModelSnapshot.cs
+++ Neighbors/Migrations/NeighborsContextModelSnapshot.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Neighbors.Data;
 using Neighbors.Models;
 
 namespace Neighbors.Migrations
