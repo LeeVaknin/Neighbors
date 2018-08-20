b5604f6228bb430ed89b23e4fe15f385064366ac
Inbal <inbalmiezels@gmail.com>
2018-08-20 22:30:15 +0300

    Added initial design and search products in index

---
+++ Neighbors/Migrations/20180820173906_2.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Neighbors.Migrations
+{
+    public partial class _2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_AspNetUsers_OwnerId",
+                table: "Product");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Product_OwnerId",
+                table: "Product");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_OwnerId",
+                table: "Product",
+                column: "OwnerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_AspNetUsers_OwnerId",
+                table: "Product",
+                column: "OwnerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
