28d3105d03ef4c28bdf842ecf26f188f5fb75d5f
caysebix <caysebix@gmail.com>
2018-09-14 13:47:49 +0300

    slight style refactoring of views

--- Neighbors/Controllers/HomeController.cs
+++ Neighbors/Controllers/HomeController.cs
@@ -18,8 +18,8 @@ namespace Neighbors.Controllers
         public IActionResult About()
         {
             // Meesages1-5 are being printed in a specific format inside the view
-            ViewData["Message1"] = "Have you ever dreamt of friends who could lend you tools instead of wasting precious money to buy it ?";
-            ViewData["Message2"] = "Neighbors are here to fulfil your dream.";
+            ViewData["Message1"] = "Have you ever dreamed of friends who could lend you tools instead of wasting precious money to buy it ?";
+            ViewData["Message2"] = "Neighbors are here to fulfill your dream.";
             ViewData["Message3"] = "Neighbors is a social platform that lets you borrow anything you wish from other members.";
             ViewData["Message4"] = "Of course, as a good neighbor one is obligated to return the tools on time and keep in a tip-top shape.";
             ViewData["Message5"] = "A user who will not follow these rules, will be fined in accordance with the terms and agreements.";
