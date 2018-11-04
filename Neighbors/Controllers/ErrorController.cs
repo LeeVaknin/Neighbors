c4ba9062ca1309f12430ae520ac2c80c14e7d0bb
Inbal <inbalmiezels@gmail.com>
2018-11-04 23:39:42 +0200

    Custom an error to invalid action from the user

--- Neighbors/Controllers/ErrorController.cs
+++ Neighbors/Controllers/ErrorController.cs
@@ -13,12 +13,19 @@ namespace Neighbors.Controllers
         public ErrorController()
         {
         }
+        [HttpGet("/Error/{errorId}")]
+        public IActionResult InvalidAction(int errorId)
+        {
+            if (errorId == 401 || errorId == 403)
+                return View();
+            return OurError();
+        }
 
         [Route("")]
         [Route("/Error")]
         public IActionResult OurError()
         {
-            return View();
+            return View("Views/Error/OurError.cshtml");
         }
 
     }
