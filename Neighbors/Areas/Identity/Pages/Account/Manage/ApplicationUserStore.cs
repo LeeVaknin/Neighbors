dfcfabf4c9ecf75dc9daf5dbaef0f943af455e10
caysebix <caysebix@gmail.com>
2018-11-09 15:36:25 +0200

    added override to find by email async (userStore)

--- Neighbors/Areas/Identity/Pages/Account/Manage/ApplicationUserStore.cs
+++ Neighbors/Areas/Identity/Pages/Account/Manage/ApplicationUserStore.cs
@@ -10,22 +10,31 @@ using System.Threading.Tasks;
 
 namespace Neighbors.Areas.Identity.Pages.Account.Manage
 {
-    public class ApplicationUserStore : UserStore<User, Role, NeighborsContext, int>
-    {
-        public ApplicationUserStore(NeighborsContext context)
-       : base(context)
-        {
-        }
-         
-        public override async Task<User> FindByIdAsync(string userId, CancellationToken cancellationToken = new CancellationToken())
-        {
-            return await Users.Include(b => b.MyBorrowed)
-                .ThenInclude(p => p.Product)
-                .ThenInclude(o => o.Owner)
-                .Include(p => p.MyProducts)
-                .ThenInclude(c => c.Category) 
-                .FirstOrDefaultAsync(u => u.Id.ToString() == userId);
-        }
+	public class ApplicationUserStore : UserStore<User, Role, NeighborsContext, int>
+	{
+		public ApplicationUserStore(NeighborsContext context)
+	   : base(context)
+		{
+		}
 
-    }
+		public override async Task<User> FindByIdAsync(string userId, CancellationToken cancellationToken = new CancellationToken())
+		{
+			return await Users.Include(b => b.MyBorrowed)
+				.ThenInclude(p => p.Product)
+				.ThenInclude(o => o.Owner)
+				.Include(p => p.MyProducts)
+				.ThenInclude(c => c.Category)
+				.FirstOrDefaultAsync(u => u.Id.ToString() == userId);
+		}
+
+		public override async Task<User> FindByEmailAsync(string userEmail, CancellationToken cancellationToken = new CancellationToken())
+		{
+			return await Users.Include(b => b.MyBorrowed)
+				.ThenInclude(p => p.Product)
+				.ThenInclude(o => o.Owner)
+				.Include(p => p.MyProducts)
+				.ThenInclude(c => c.Category)
+				.FirstOrDefaultAsync(u => u.Email.ToString() == userEmail);
+		}
+	}
 }
