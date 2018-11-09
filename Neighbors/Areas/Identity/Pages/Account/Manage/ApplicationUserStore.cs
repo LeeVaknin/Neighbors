2a6b92bf99bc2a643ab4ca5c7d09feadea099d45
caysebix <caysebix@gmail.com>
2018-11-09 15:49:05 +0200

    removed the find by email async

--- Neighbors/Areas/Identity/Pages/Account/Manage/ApplicationUserStore.cs
+++ Neighbors/Areas/Identity/Pages/Account/Manage/ApplicationUserStore.cs
@@ -27,14 +27,5 @@ namespace Neighbors.Areas.Identity.Pages.Account.Manage
 				.FirstOrDefaultAsync(u => u.Id.ToString() == userId);
 		}
 
-		public override async Task<User> FindByEmailAsync(string userEmail, CancellationToken cancellationToken = new CancellationToken())
-		{
-			return await Users.Include(b => b.MyBorrowed)
-				.ThenInclude(p => p.Product)
-				.ThenInclude(o => o.Owner)
-				.Include(p => p.MyProducts)
-				.ThenInclude(c => c.Category)
-				.FirstOrDefaultAsync(u => u.Email.ToString() == userEmail);
-		}
 	}
 }
